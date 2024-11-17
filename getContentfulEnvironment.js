bccb5c253166bb580c0b4e68c7cbf7668ca1641a
Stephanie Cobbold <steph.cobbold@gmail.com>
2024-11-17 02:04:17 +0000

    build(deps): bump deps

--- getContentfulEnvironment.js
+++ getContentfulEnvironment.js
@@ -1,13 +1,22 @@
 const contentfulManagement = require('contentful-management');
 
-const getContentfulEnvironment = () => {
+const getContentfulEnvironment = async () => {
+  const { CONTENTFUL_ENVIRONMENT, CONTENTFUL_MANAGEMENT_ACCESS_TOKEN, CONTENTFUL_SPACE_ID } = process.env;
+
   const contentfulClient = contentfulManagement.createClient({
-    accessToken: process.env.CONTENTFUL_MANAGEMENT_ACCESS_TOKEN,
+    accessToken: CONTENTFUL_MANAGEMENT_ACCESS_TOKEN,
   });
 
-  return contentfulClient
-    .getSpace(process.env.CONTENTFUL_SPACE_ID)
-    .then((space) => space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT));
+  try {
+    const space = await contentfulClient.getSpace(CONTENTFUL_SPACE_ID);
+
+    return space.getEnvironment(CONTENTFUL_ENVIRONMENT);
+  } catch (error) {
+    console.error(
+      `Couldn\'t fetch environment "${CONTENTFUL_ENVIRONMENT}" for space "${CONTENTFUL_SPACE_ID}". Reason:`,
+      JSON.stringify(error)
+    );
+  }
 };
 
 module.exports = getContentfulEnvironment;
