f6da2c034e57c5d552933fc7b24de7d8dc7aad1f
Stephanie Cobbold <steph.cobbold@gmail.com>
2024-12-05 12:17:50 +0000

    Add keystatic cms

---
+++ keystatic.config.ts
@@ -0,0 +1,73 @@
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  collections: {
+    stories: collection({
+      label: 'Stories',
+      slugField: 'title',
+      path: 'src/content/stories/*',
+      format: {
+        contentField: 'summary'
+      },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        summary: fields.markdoc({ label: 'Summary' }),
+        image: fields.image({
+          label: 'Cover image for the story',
+        }),
+        chapters: fields.array(
+          fields.relationship({
+            label: 'Chapters',
+            collection: 'chapters',
+          }),
+          {
+            label: 'Chapters',
+            itemLabel: (props) => props.value as string,
+          }
+        ),
+      },
+    }),
+    chapters: collection({
+      label: 'Chapters',
+      slugField: 'title',
+      path: 'src/content/chapters/*',
+      format: {
+        contentField: 'summary',
+      },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        summary: fields.markdoc({ label: 'Summary' }),
+        image: fields.image({
+          label: 'Cover image for the chapter',
+        }),
+        pages: fields.array(
+          fields.relationship({
+            label: 'Pages',
+            collection: 'pages',
+          }),
+          { label: 'Pages' }
+        ),
+      },
+    }),
+    pages: collection({
+      label: 'Pages',
+      slugField: 'slug',
+      path: 'src/content/pages/*',
+      format: {
+        contentField: 'copy',
+      },
+      schema: {
+        slug: fields.slug({ name: { label: 'Page Slug' } }),
+        image: fields.image({
+          label: 'Cover image for the page',
+        }),
+        copy: fields.markdoc({
+          label: 'Page copy',
+        }),
+      },
+    }),
+  },
+});
